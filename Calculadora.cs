7566b7b351cec75526bb4ac1ad650c9a39f090fc
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-01 14:35:37 -0300

    Versão 3: classe Calculadora com método estático

--- Calculadora.cs
+++ Calculadora.cs
@@ -8,15 +8,15 @@ namespace staticMembersAtributesMetods
 {
     internal class Calculadora
     {
-        public double Pi = 3.14;
+        public static double Pi = 3.14;
 
 
 
-        public double Circunferencia(double r)
+        public static double Circunferencia(double r)
         {
             return 2.0 * Pi * r;
         }
-        public double Volume(double r)
+        public static double Volume(double r)
         {
             return 4.0 / 3.0 * Pi * r * r * r;
         }
