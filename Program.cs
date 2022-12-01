4e2084eae1309bbbb2083425e2e869a82474a55a
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-01 14:31:56 -0300

    Versão 2: classe Calculadora com membros de instância

--- Program.cs
+++ Program.cs
@@ -8,30 +8,18 @@ casas decimais.*/
 • Nota: dentro de um método estático você não pode chamar membros de
 instância da mesma classe.*/
 
+using staticMembersAtributesMetods;
 using System.Globalization;
-double pi = 3.14;
+
+Calculadora calc = new Calculadora();
+
 
 Console.Write("Entre com o valor do raio: ");
 double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-double circ = Circunferencia(raio);
-double volume = Volume(raio);
+double circ = calc.Circunferencia(raio);
+double volume = calc.Volume(raio);
 
 Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
 Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-Console.WriteLine("Valor de PI: " + pi);
-
-
-//Console.WriteLine("Valor de PI: " + );
-
-static double Circunferencia(double r, double pi = 3.14)
-{
-    return 2.0 * pi * r;
-}
-static double Volume(double r, double pi = 3.14)
-{
-    return 4.0 / 3.0 * pi * r * r * r;
-}
-
-
-
+Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
